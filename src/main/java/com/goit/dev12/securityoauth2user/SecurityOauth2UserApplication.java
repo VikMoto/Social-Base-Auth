fd2b23e2d4d3ceaac2b1ade3b2ac10dd5ee8ed69
petroz <petroz@ext.inditex.com>
2023-08-05 12:44:19 +0300

    Initial commit

---
+++ src/main/java/com/goit/dev12/securityoauth2user/SecurityOauth2UserApplication.java
@@ -0,0 +1,13 @@
+package com.goit.dev12.securityoauth2user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2UserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecurityOauth2UserApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/goit/dev12/securityoauth2user/SecurityOauth2UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goit.dev12.securityoauth2user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauth2UserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
